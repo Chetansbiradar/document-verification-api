1258319c071ffc82d9f0f38dab461dd0fb0370ec
Chetansbiradar <chetanhattoor@gmail.com>
2022-10-28 05:01:22 +0530

    token from cookie, verifyTokenAndAdmin modified

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,19 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req,res,next)=>{
-    const authHeader = req.headers.token;
-    if(authHeader){
-        jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
-            if(err){
-                res.status(403).json("Invalid token, wrong token or expired");
-            }else{
-                req.user = user;
-                next();
-            }
-        })
-    }else{
-        return res.status(401).json("Your are not authenticated");
-    }
+    const token  = req.cookies.auth_token;
+    if(!token) return res.status(403).json({"error":{"message":"Token Missing, did you get one?"}})
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+        if(err){
+            res.status(403).json("Invalid token or expired");
+        }else{
+            req.user = user;
+            next();
+        }
+    })
 };
 
 const verifyTokenAndAuthorization = (req,res,next)=>{
@@ -28,10 +26,13 @@ const verifyTokenAndAuthorization = (req,res,next)=>{
 
 const verifyTokenAndAdmin = (req,res,next)=>{
     verifyToken(req,res,()=>{
-        if(req.user.id === req.params.id || req.user.accessLevel=== 2 ){
+        console.log(req.user)
+        if(req.user.accessLevel === 2){
+            const d = new Date();
+            console.log("🔋Admin route accessed on: ",d," 🕵️")
             next();
         }else{
-            res.status(403).json("Your are not allowed to do this");
+            res.status(403).json({"error":{"message":"Your are not allowed to do this, probably you shouldn't be on this route"}});
         }
     })
 };

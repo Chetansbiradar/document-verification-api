bb1a8af2f7837c27dcf779aa6c6b8d49b8b5a092
Chetansbiradar <chetanhattoor@gmail.com>
2022-10-27 14:34:26 +0530

    changed Middleware

--- routes/user.js
+++ routes/user.js
@@ -21,17 +21,27 @@ router.put("/:id",verifyTokenAndAuthorization,async (req,res)=>{
 });
 
 // DELETE
-router.delete("/:id",verifyTokenAndAuthorization,async(req,res)=>{
+router.delete("/:id",verifyTokenAndAdmin,async(req,res)=>{
     try {
         await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted");
+        res.status(200).json("Student has been deleted");
     } catch (error) {
         res.status(500).json(error);
     }
 });
 
 // GET USER
-router.delete("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
+router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        const {password,...others} = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
     try {
         const user = await User.findById(req.params.id);
         const {password,...others} = user._doc;

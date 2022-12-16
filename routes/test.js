53a5528f072d9b176f6acada034de630414533fc
Chetansbiradar <chetanhattoor@gmail.com>
2022-12-16 22:40:54 +0530

    test file

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 const ReportCard = require("../models/ReportCard");
 const { verifyTokenAndAdmin } = require("./verifyToken");
 
+const multer = require("multer");
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
+
 // @route   POST api/universities
 // @desc    Create a university
 // @access  verifyTokenAndAdmin
@@ -16,29 +21,50 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
     dateOfIssue: req.body.dateOfIssue,
     monthOfIssue: req.body.monthOfIssue,
     yearOfIssue: req.body.yearOfIssue,
-    subjects:[],
+    subjects: req.body.subjects,
     creditsDuringSem: req.body.creditsDuringSem,
     totalCredits: req.body.totalCredits,
     sgpa: req.body.sgpa,
     cgpa: req.body.cgpa,
   });
-  req.body.subjects.forEach((subject) => {
-    reoprtCard.subjects.push({
-      subName: subject.subName,
-      subCode: subject.subCode,
-      credits: subject.credits,
-      grade: subject.grade,
-    });
-  })
   try {
-    const checkCardExists = await ReportCard.findOne({srn: req.body.srn});
-    if(checkCardExists) {
-        const upadteCard = await checkCardExists.updateOne({$set: reoprtCard});
+    const checkCardExists = await ReportCard.findOne({ srn: req.body.srn });
+
+    //update if exists
+    if (checkCardExists) {
+      console.log("Card already exists");
+      checkCardExists.updateOne({ srn: req.body.srn }, { $set: reoprtCard });
     }
+
     const report = await reoprtCard.save();
     if (!report)
       throw Error("Something went wrong while saving the Report Card");
     res.status(200).json(report);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+router.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    const {
+      srn,
+      name,
+      programOfStudy,
+      semester,
+      monthOfExam,
+      yearOfExam,
+      dateOfIssue,
+      monthOfIssue,
+      yearOfIssue,
+      subjects,
+      creditsDuringSem,
+      totalCredits,
+      sgpa,
+      cgpa
+    } = req.body;
+    const file = req.file;
+    res.status(200).json({ msg: "success" });
   } catch (e) {
     res.status(400).json({ msg: e.message });
   }

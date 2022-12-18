c10842e360db43c902a2fe29ec2ebe1ccd90b738
Chetansbiradar <chetanhattoor@gmail.com>
2022-12-18 11:57:27 +0530

    added new routes [reset-password, refresh-token..]

--- routes/auth.js
+++ routes/auth.js
@@ -1,69 +1,168 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const RegisterValidator = require("./validation");
-
+const createToken = require("../utils/createToken");
+const { verifyToken } = require("../utils/verifyToken");
 
 //Register
-router.post("/register",async (req,res)=>{
-    
-    //Validate
-    const {error} = RegisterValidator(req.body)
-    if(error){
-        return res.status(200).json({"error":{message:error.details[0].message}})
-    }
+router.post("/register", async (req, res) => {
+  try {
+    //destruture req.body
+    const { name, email, password } = req.body;
 
-    //check if email exits
-    const emailExits = await User.findOne({email:req.body.email});
-    if(emailExits){
-        return res.status(400).json({"error":{message:"email already exists"}})
-    }
+    //check if all fields are filled
+    if (!name || !email || !password) throw new Error("Please fill all fields");
 
-    //register user if all good
-    const salt = await bcrypt.genSalt(10); //salting
-    const hasedPassword = await bcrypt.hash(req.body.password,salt) //hashing
-    console.log(req.params);
+    //check if email exits in db
+    const emailExits = await User.findOne({ email });
+    if (emailExits) throw new Error("Email already exists");
+
+    const salt = await bcrypt.genSalt(10); //salting password
+    if (!salt) throw new Error("Something went wrong while salting password");
+
+    const hashedPassword = await bcrypt.hash(req.body.password, salt); //hashing password
+    if (!hashedPassword)
+      throw new Error("Something went wrong while hashing password");
+
     const newUser = new User({
-        name:req.body.name,
-        email:req.body.email,
-        password: hasedPassword
-        });
-        try{
-            const savedUser = await newUser.save();
-            const {accessLevel,password,...savedUserResponse} = savedUser._doc;
-            res.status(201).json(savedUserResponse);
-        }catch(err){
-            res.status(500).json({error:"Something went wrong!"});
-            console.log(err);
-    }
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    if (!savedUser) throw Error("Something went wrong while saving the user");
+
+    const accessToken = createToken(savedUser, process.env.JWT_SECRET, "3d");
+
+    res
+      .cookie("token", accessToken, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "User registered successfully",
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
-
 //Login
-router.post("/login",async(req,res)=>{
-    try {
-        const user = await User.findOne({email: req.body.email});
-        if(!user) return res.status(401).json("Invalid Credentialse e");
-        const password = await bcrypt.compare(req.body.password,user.password);
-        if(!password) return res.status(401).json("Invalid Credentials! p");
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        const accessToken = jwt.sign(
-            {
-                id:user._id,
-                name:user.name,
-                email:user.email,
-                accessLevel:user.accessLevel
-            },
-            process.env.JWT_SECRET,
-            {   expiresIn:"3d" 
-            }
-        );
-        res.status(200).json({accessToken:accessToken,"message":"Login Successful"});
+    if (!email || !password) throw new Error("Please fill all fields");
 
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("User does not exist");
 
-module.exports = router
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error("Invalid Credentials");
+
+    const accessToken = await createToken(user, process.env.JWT_SECRET, "3d");
+    if (!accessToken) throw new Error("Something went wrong while creating token");
+    res
+      .cookie("token", accessToken, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "User logged in successfully",
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//Logout
+router.get("/logout", (req, res) => {
+  res
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    })
+    .status(200)
+    .json({
+      success: true,
+      message: "User logged out successfully",
+    });
+});
+
+//refresh token
+router.get("/refresh-token", verifyToken, async (req, res) => {
+  try {  
+    const user = await User.findById(req.user._id);
+    if (!user) throw new Error("User not found");
+  
+    const newToken = await createToken(user, process.env.JWT_SECRET, "3d");
+    if (!newToken) throw new Error("Something went wrong while creating token");
+  
+    res
+    .cookie("token", newToken, {
+      httpOnly: true,
+    })
+    .status(200)
+    .json({
+      success: true,
+      accessLevel: user.accessLevel,
+      message: "Token refreshed successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+
+});
+
+//reset password
+router.post("/reset-password", verifyToken, async (req, res) => {
+  try {
+
+    const user = await User.findById(req.user._id);
+    if (!user) throw new Error("User not found");
+
+    const { oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) throw new Error("Please fill all fields");
+
+    const oldPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
+    if (!oldPasswordCorrect) throw new Error("Invalid credentials");
+
+    const salt = await bcrypt.genSalt(10); //salting password
+    if (!salt) throw new Error("Something went wrong while salting password");
+
+    const hashedPassword = await bcrypt.hash(newPassword, salt); //hashing password
+    if (!hashedPassword)
+      throw new Error("Something went wrong while hashing password");
+
+    user.password = hashedPassword;
+    const savedUser = await user.save();
+    if (!savedUser) throw Error("Something went wrong while saving the user");
+
+    res.status(200).json({
+      success: true,
+      accessLevel: user.accessLevel,
+      message: "Password reset successfully",
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+module.exports = router;

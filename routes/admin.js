1c1e3e40166591fd20a763bc8c2994f3fa96286f
Chetansbiradar <chetanhattoor@gmail.com>
2022-12-18 11:57:58 +0530

    refactored admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,54 +1,36 @@
 const router = require("express").Router();
-const User = require("../models/User");
-const { verifyTokenAndAdmin } = require("./verifyToken");
 
-// getRegisteredUsers //accessLevel=0
-router.get("/registeredusers", verifyTokenAndAdmin, async (req, res, next) => {
-  const users = await User.find({ accessLevel: 0 });
-  let destructeredUsers = new Array();
-  if (users) {
-    //remove password from users
-    for (i = 0; i < users.length; i++) {
-      const { password, ...other } = users[i]._doc;
-      destructeredUsers.push(other);
-    }
-    return res.status(200).json(destructeredUsers);
+const User = require("../models/User");
+const { verifyTokenAndAdmin } = require("../utils/verifyToken");
+
+//Get all users
+router.get("/get-all-users", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const users = await User.find().select("-password").sort({ name: 1 });
+    if (!users) throw new Error("No users found");
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
   }
 });
 
-// giveAccessToUser //toMake accessLevel=0/1/2
-router.get(
-  "/upgradeuser/:id/:accessLevel",
-  verifyTokenAndAdmin,
-  async (req, res, next) => {
-    try {
-      const user = await User.findById(req.params.id);
-      if (!user)
-        return res.status(400).json({ error: { message: "Invalid User ID" } });
-      if (req.params.accessLevel > 2 || req.params.accessLevel < 0)
-        return res
-          .status(400)
-          .json({ error: { message: "Invalid Access Level" } });
-      const updated = await User.updateOne(
-        { _id: req.params.id },
-        { accessLevel: req.params.accessLevel }
-      );
-      if (updated.modifiedCount === 1)
-        return res.status(202).json({ message: "User Updated Successfully" });
-    } catch (error) {
-      return res
-        .status(400)
-        .json({
-          error: {
-            message: "No User found by that ID or Coudn't update the user",
-          },
-        });
-    }
+// getRegisteredUsers where accessLevel = 0
+router.get("/get-users-0", verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const users = await User.find({ accessLevel: 0 }).select("-password");
+    if (!users) throw new Error("No users found");
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
   }
-);
-// getRegisteredAuth //accessLevel=1
+});
+
+// getRegisteredAuth where accessLevel = 1 or 2
 router.get(
-  "/registeredauthuser",
+  "/get-upgraded-users",
   verifyTokenAndAdmin,
   async (req, res, next) => {
     try {
@@ -72,5 +54,58 @@ router.get(
     }
   }
 );
+
+// upgrade or downgrade user accessLevel
+router.get(
+  "/update-access-level/:id/:accessLevel",
+  verifyTokenAndAdmin,
+  async (req, res, next) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) throw new Error("User not found");
+
+      if (req.params.accessLevel > 2 || req.params.accessLevel < 0)
+        throw new Error("Invalid access level");
+
+      const updated = await User.updateOne(
+        { _id: req.params.id },
+        { accessLevel: req.params.accessLevel }
+      );
+
+      if (updated.modifiedCount === 1)
+        return res
+          .status(202)
+          .json({ success: true, message: "User Updated Successfully" });
+      else throw new Error("User not updated");
+    } catch (err) {
+      return res.status(400).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  }
+);
+
+// delete user
+router.get("/delete-user/:id", verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) throw new Error("User not found");
+
+    const deleted = await User.deleteOne({ _id: user._id });
+    if (!deleted) throw new Error("User not deleted");
+
+    if (deleted.deletedCount === 1)
+      return res
+        .status(202)
+        .json({ success: true, message: "User Deleted Successfully" });
+    else throw new Error("User not deleted");
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
